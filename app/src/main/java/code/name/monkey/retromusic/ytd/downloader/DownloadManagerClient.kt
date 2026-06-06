12304b93d7d2b280e726263c1c11aa01293bbf88
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-06 18:46:10 -0500

    Update DownloadManagerClient.kt

--- app/src/main/java/code/name/monkey/retromusic/ytd/downloader/DownloadManagerClient.kt
+++ app/src/main/java/code/name/monkey/retromusic/ytd/downloader/DownloadManagerClient.kt
@@ -1,1 +1,32 @@
+package code.name.monkey.retromusic.ytd.downloader
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
+
+object DownloadManagerClient {
+
+    fun download(
+        context: Context,
+        url: String,
+        fileName: String
+    ): Long {
+
+        val request = DownloadManager.Request(Uri.parse(url))
+            .setTitle(fileName)
+            .setDescription("Descargando contenido...")
+            .setNotificationVisibility(
+                DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED
+            )
+            .setAllowedOverMetered(true)
+            .setAllowedOverRoaming(true)
+            .setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS,
+                fileName
+            )
+
+        val manager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        return manager.enqueue(request)
+    }
+}
