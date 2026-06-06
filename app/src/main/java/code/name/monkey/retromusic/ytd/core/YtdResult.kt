d64bb091e2f21e2c01dc69fe59cc82ebf58ca8bf
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-06 18:44:20 -0500

    Update YtdResult.kt

--- app/src/main/java/code/name/monkey/retromusic/ytd/core/YtdResult.kt
+++ app/src/main/java/code/name/monkey/retromusic/ytd/core/YtdResult.kt
@@ -1,1 +1,18 @@
+package code.name.monkey.retromusic.ytd.models
 
+sealed class YtdResult {
+
+    data class Video(
+        val url: String,
+        val quality: String
+    ) : YtdResult()
+
+    data class Audio(
+        val url: String,
+        val mime: String = "audio/mp4"
+    ) : YtdResult()
+
+    data class Error(
+        val message: String
+    ) : YtdResult()
+}
