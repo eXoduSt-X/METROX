6680ca7565e4765721160db3dbe56f03c95bb646
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-06 18:55:31 -0500

    Update YouTubeExtractor.kt

--- app/src/main/java/code/name/monkey/retromusic/ytd/extractor/YouTubeExtractor.kt
+++ app/src/main/java/code/name/monkey/retromusic/ytd/extractor/YouTubeExtractor.kt
@@ -1,1 +1,19 @@
+package code.name.monkey.retromusic.ytd.extractor
 
+import org.schabi.newpipe.extractor.NewPipe
+import org.schabi.newpipe.extractor.services.youtube.YoutubeService
+import org.schabi.newpipe.extractor.stream.StreamInfo
+import java.net.URL
+
+object YoutubeExtractor {
+
+    init {
+        NewPipe.init(null)
+    }
+
+    fun extract(url: String): StreamInfo {
+        val service = NewPipe.getService(0) as YoutubeService
+        val streamUrl = service.getStreamUrl(url)
+        return StreamInfo.getInfo(streamUrl)
+    }
+}
