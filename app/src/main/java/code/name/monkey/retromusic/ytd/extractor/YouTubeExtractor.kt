4631071617e31266500517dc0686d05cf1ec9378
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-06 19:08:17 -0500

    Simplify extract method in YouTubeExtractor
    Refactor YouTubeExtractor to use URL directly for stream info.

--- app/src/main/java/code/name/monkey/retromusic/ytd/extractor/YouTubeExtractor.kt
+++ app/src/main/java/code/name/monkey/retromusic/ytd/extractor/YouTubeExtractor.kt
@@ -1,9 +1,7 @@
 package code.name.monkey.retromusic.ytd.extractor
 
 import org.schabi.newpipe.extractor.NewPipe
-import org.schabi.newpipe.extractor.services.youtube.YoutubeService
 import org.schabi.newpipe.extractor.stream.StreamInfo
-import java.net.URL
 
 object YoutubeExtractor {
 
@@ -12,8 +10,6 @@ object YoutubeExtractor {
     }
 
     fun extract(url: String): StreamInfo {
-        val service = NewPipe.getService(0) as YoutubeService
-        val streamUrl = service.getStreamUrl(url)
-        return StreamInfo.getInfo(streamUrl)
+        return StreamInfo.getInfo(url)
     }
 }
