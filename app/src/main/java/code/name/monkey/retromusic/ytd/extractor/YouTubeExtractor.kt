23ea95b1ece5c4b7f178259f25f858ef32502fde
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-06 19:33:44 -0500

    Update YouTubeExtractor.kt

--- app/src/main/java/code/name/monkey/retromusic/ytd/extractor/YouTubeExtractor.kt
+++ app/src/main/java/code/name/monkey/retromusic/ytd/extractor/YouTubeExtractor.kt
@@ -2,8 +2,7 @@ package code.name.monkey.retromusic.ytd.extractor
 
 object YoutubeExtractor {
 
-    fun extract(url: String): Any? {
-        // CI SAFE: todavía no dependemos de NewPipe directo
-        return null
+    fun extract(url: String): String {
+        return url
     }
 }
