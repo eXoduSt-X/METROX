895be23cee18668093f0f2d7222fa26c39192e25
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-08 04:14:47 -0500

    Refactor HomeBinding by cleaning up unused code
    Removed unused variables and comments in HomeBinding class.

--- app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeBinding.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeBinding.kt
@@ -4,19 +4,14 @@ import code.name.monkey.retromusic.databinding.FragmentHomeBinding
 
 class HomeBinding(val binding: FragmentHomeBinding) {
     val root = binding.root
-    
-    // Cabecera y Toolbar
     val appBarLayout = binding.appBarLayout
     val toolbar = binding.appBarLayout.toolbar
     val contentContainer = binding.contentContainer
     val container = binding.container
-    
-    // Perfil
     val titleWelcome = binding.imageLayout.titleWelcome
     val bannerImage = binding.imageLayout.bannerImage
     val userImage = binding.imageLayout.userImage
     
-    // Playlists (Acceso a través de homeContent)
     private val homeContent = binding.homeContent
     val absPlaylists = homeContent.absPlaylists
     val lastAdded = homeContent.absPlaylists.lastAdded
@@ -24,18 +19,11 @@ class HomeBinding(val binding: FragmentHomeBinding) {
     val actionShuffle = homeContent.absPlaylists.actionShuffle
     val history = homeContent.absPlaylists.history
     
-    // Video y WebView
-    val youtubeWebView = homeContent.youtubeWebView
     val videoDownloadContainer = homeContent.videoDownloadContainer
     val videoDownloadView = homeContent.videoDownloadView
-    val btnDownloadFloating = homeContent.btnDownloadFloating
+    val youtubeWebView = homeContent.youtubeWebView
     val btnLoadLocalVideo = homeContent.btnLoadLocalVideo
     
-    // Botones adicionales (si tu XML los tiene)
-    val btnStartDownload = homeContent.btnStartDownload
-    
-    // NOTA: Si en el futuro agregas los IDs al XML, 
-    // puedes descomentar estas líneas:
-    // val etDownloadUrl = homeContent.etDownloadUrl
-    // val btnClearUrl = homeContent.btnClearUrl
+    // Comentado para evitar el error de "Unresolved Reference"
+    // val btnDownloadFloating = homeContent.btnDownloadFloating
 }
