5cdfc9a67af20c9dfcfa3d8f522263b06e4555d0
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-08 02:38:36 -0500

    Refactor HomeBinding for direct view access
    Refactor HomeBinding to access views directly from the root of FragmentHomeBinding instead of through homeContent.

--- app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeBinding.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeBinding.kt
@@ -1,40 +1,28 @@
 package code.name.monkey.retromusic.fragments.home
 
 import code.name.monkey.retromusic.databinding.FragmentHomeBinding
+import android.view.View
 
-class HomeBinding(
-    private val binding: FragmentHomeBinding
-) {
+class HomeBinding(binding: FragmentHomeBinding) {
     val root = binding.root
     val container = binding.container
     val contentContainer = binding.contentContainer
     val appBarLayout = binding.appBarLayout
     val toolbar = binding.appBarLayout.toolbar
     
-    // Referencia directa al layout incluido 'home_content'
-    private val homeContent = binding.homeContent
-
-    val bannerImage = binding.imageLayout.bannerImage
-    val userImage = binding.imageLayout.userImage
-    val titleWelcome = binding.imageLayout.titleWelcome
+    // Buscamos los IDs directamente en el root del FragmentHomeBinding
+    val lastAdded = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.lastAdded)
+    val topPlayed = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.topPlayed)
+    val actionShuffle = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.actionShuffle)
+    val history = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.history)
     
-    // Acceso simplificado a los elementos dentro del include 'abs_playlists'
-    val lastAdded = homeContent.absPlaylists.lastAdded
-    val topPlayed = homeContent.absPlaylists.topPlayed
-    val actionShuffle = homeContent.absPlaylists.actionShuffle
-    val history = homeContent.absPlaylists.history
-    
-    // --- COMPONENTES DEL PANEL DE VIDEO (Dentro de homeContent) ---
-    // NOTA: Si en home_content.xml definiste los botones dentro de un layout, 
-    // el binding los expone directamente a través de 'homeContent'.
-    val etDownloadUrl = homeContent.etDownloadUrl
-    val btnClearUrl = homeContent.btnClearUrl
-    val btnStartDownload = homeContent.btnStartDownload
-    val btnLoadLocalVideo = homeContent.btnLoadLocalVideo
-    val videoDownloadContainer = homeContent.videoDownloadContainer
-    val videoDownloadView = homeContent.videoDownloadView
+    val etDownloadUrl = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.etDownloadUrl)
+    val btnClearUrl = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.btnClearUrl)
+    val btnStartDownload = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.btnStartDownload)
+    val btnLoadLocalVideo = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.btnLoadLocalVideo)
+    val videoDownloadContainer = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.videoDownloadContainer)
+    val videoDownloadView = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.videoDownloadView)
 
-    // --- NAVEGADOR YOUTUBE ---
-    val youtubeWebView = homeContent.youtubeWebView
-    val btnDownloadFloating = homeContent.btnDownloadFloating
+    val youtubeWebView = binding.root.findViewById<android.webkit.WebView>(code.name.monkey.retromusic.R.id.youtubeWebView)
+    val btnDownloadFloating = binding.root.findViewById<View>(code.name.monkey.retromusic.R.id.btnDownloadFloating)
 }
