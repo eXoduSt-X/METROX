79526f044fcea4623847aae55ff5ff3286be4ac9
eXoduSt-X <the.exoskeleton@gmail.com>
2026-06-09 02:04:55 -0500

    Remove video seek bar change listener
    Removed video seek bar listener initialization code.

--- app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeFragment.kt
@@ -114,16 +114,6 @@ class HomeFragment : AbsMainActivityFragment(R.layout.fragment_home), IScrollHel
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     _binding = FragmentHomeBinding.bind(view)
-// Inicializar la barra de progreso
-   binding.homeContent.videoSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-      override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-         if (fromUser) {
-             binding.homeContent.videoPlayer.seekTo(progress)
-         }
-     }
-    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-})
 
 // Actualizar la barra mientras el video corre (dentro de un Handler o Timer)
 // Puedes añadir esto al final de tu 'updateSubtitleTask' que ya tienes:
