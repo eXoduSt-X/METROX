41ae589462eaba952c01678b3ed64b0c8fb3e7be
eXoduSt-X <the.exoskeleton@gmail.com>
2026-07-18 05:45:58 -0500

    Add StretchVideoView for fullscreen video display
    Created a custom VideoView that stretches video to fit its container, ignoring the original aspect ratio.

---
+++ app/src/main/java/code/name/monkey/retromusic/fragments/home/StretchVideoView.kt
@@ -0,0 +1,24 @@
+package code.name.monkey.retromusic.fragments.home
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.VideoView
+
+/**
+ * VideoView que ignora la relación de aspecto original del video y ocupa
+ * exactamente el tamaño que le da su contenedor (estira/aplasta el video
+ * si hace falta). Usado en fullscreen para que el área visible sea siempre
+ * la misma sin importar la resolución del video cargado.
+ */
+class StretchVideoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : VideoView(context, attrs) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(
+            View.MeasureSpec.getSize(widthMeasureSpec),
+            View.MeasureSpec.getSize(heightMeasureSpec)
+        )
+    }
+}
