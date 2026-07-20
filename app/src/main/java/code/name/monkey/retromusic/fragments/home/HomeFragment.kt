cba4ce21c92246e03ca219941b2e444c076c83f5
eXoduSt-X <the.exoskeleton@gmail.com>
2026-07-19 19:24:35 -0500

    Fix subtitle overlay assignment method

--- app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/home/HomeFragment.kt
@@ -100,7 +100,8 @@ class HomeFragment : AbsMainActivityFragment(R.layout.fragment_home), IScrollHel
                 val player = binding.homeContent.videoPlayer
                 val currentPos = player.currentPosition
                 val currentSub = subtitleList.find { currentPos.toLong() in it.startTime..it.endTime }
-                binding.homeContent.tvSubtitleOverlay.text = currentSub?.text ?: ""
+               // binding.homeContent.tvSubtitleOverlay.text = currentSub?.text ?: ""
+               (binding.homeContent.tvSubtitleOverlay as TextView).text = currentSub?.text ?: ""
                 binding.homeContent.videoSeekBar.max = player.duration
                 binding.homeContent.videoSeekBar.progress = currentPos
                 binding.homeContent.tvCurrentTime.text = formatTime(currentPos)
